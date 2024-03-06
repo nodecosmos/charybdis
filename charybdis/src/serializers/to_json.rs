c1e07e4c9d716a69ce1dfc82e37a391b990905dd
Goran Brkuljan <goran.brkuljan@gmail.com>
2024-03-06 19:39:31 +0100

    fix: to_json

--- charybdis/src/serializers/to_json.rs
+++ charybdis/src/serializers/to_json.rs
@@ -3,11 +3,11 @@ use crate::model::BaseModel;
 use serde::Serialize;
 
 pub trait ToJson<T: BaseModel + Serialize> {
-    fn from_json(json: &str) -> Result<String, CharybdisError>;
+    fn to_json(&self) -> Result<String, CharybdisError>;
 }
 
 impl<T: BaseModel + Serialize> ToJson<T> for T {
-    fn from_json(json: &str) -> Result<String, CharybdisError> {
-        serde_json::to_string(json).map_err(|e| CharybdisError::JsonError(e))
+    fn to_json(&self) -> Result<String, CharybdisError> {
+        serde_json::to_string(&self).map_err(|e| CharybdisError::JsonError(e))
     }
 }
