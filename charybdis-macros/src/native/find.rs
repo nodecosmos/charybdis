ce6c823946d87d6a32b22d884641117e2e15bd88
Goran Brkuljan <goran.brkuljan@gmail.com>
2024-03-19 20:31:40 +0100

    refactor: add comments

--- charybdis-macros/src/native/find.rs
+++ charybdis-macros/src/native/find.rs
@@ -51,7 +51,6 @@ pub(crate) fn find_by_primary_keys_functions(
             where_placeholders(&current_fields)
         );
         let find_by_fun_name_str = format!("find_by_{}", current_field_names.join("_and_"));
-
         let find_by_fun_name = syn::Ident::new(&find_by_fun_name_str, proc_macro2::Span::call_site());
         let is_complete_pk = current_fields.len() == primary_key_stack.len();
         let arguments = struct_fields_to_fn_args(
@@ -59,6 +58,7 @@ pub(crate) fn find_by_primary_keys_functions(
             fields.db_fields.clone(),
             current_field_names.clone(),
         );
+
         let generated_func;
 
         if is_complete_pk {
@@ -97,15 +97,14 @@ pub(crate) fn find_first_by_primary_keys_functions(
             .iter()
             .map(|field| field.name.clone())
             .collect::<Vec<String>>();
-
         let query_str = format!(
             "SELECT {} FROM {} WHERE {}",
             comma_sep_cols,
             table_name,
             where_placeholders(&current_fields)
         );
+        // find first
         let find_by_fun_name_str = format!("find_first_by_{}", current_field_names.join("_and_"));
-
         let find_by_fun_name = syn::Ident::new(&find_by_fun_name_str, proc_macro2::Span::call_site());
         let arguments = struct_fields_to_fn_args(
             struct_name.to_string(),
@@ -114,6 +113,7 @@ pub(crate) fn find_first_by_primary_keys_functions(
         );
         let generated_func = find_one_generated_fn(&find_by_fun_name, &arguments, struct_name, &query_str);
 
+        // maybe find first
         let maybe_find_by_fun_name_str = format!("maybe_find_first_by_{}", current_field_names.join("_and_"));
         let maybe_find_by_fun_name = syn::Ident::new(&maybe_find_by_fun_name_str, proc_macro2::Span::call_site());
         let maybe_generated_func =
