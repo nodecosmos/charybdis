c97ffa4604b13630843910b9ffa63acd2eaee3f8
Goran Brkuljan <goran.brkuljan@gmail.com>
2024-04-23 12:44:41 +0200

    update: remove unused dep & method

--- charybdis-parser/src/traits/array.rs
+++ charybdis-parser/src/traits/array.rs
@@ -1,21 +1,12 @@
 use quote::ToTokens;
-use std::collections::HashSet;
 use syn::ExprArray;
 
 pub(crate) trait ToStringCollection {
     fn to_vec(self) -> Vec<String>;
-    fn to_set(self) -> HashSet<String>;
 }
 
 impl ToStringCollection for ExprArray {
     fn to_vec(self) -> Vec<String> {
-        self.elems
-            .into_iter()
-            .map(|elem| elem.to_token_stream().to_string())
-            .collect()
-    }
-
-    fn to_set(self) -> HashSet<String> {
         self.elems
             .into_iter()
             .map(|elem| elem.to_token_stream().to_string())
