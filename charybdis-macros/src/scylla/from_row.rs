b91a0ecb9e06f3cbc51cfd66f14c476c3b509f15
Goran Brkuljan <goran.brkuljan@gmail.com>
2024-11-14 13:07:39 +0100

    update: update from_row implementation

--- charybdis-macros/src/scylla/from_row.rs
+++ charybdis-macros/src/scylla/from_row.rs
@@ -3,24 +3,22 @@ use syn::ImplItem;
 
 use charybdis_parser::fields::CharybdisFields;
 
+/// Generates the `from_row` method for the struct. We have manually implemented this method
+/// because we need to enable non-db fields to be set to their default values.
 pub(crate) fn from_row(struct_name: &syn::Ident, fields: &CharybdisFields) -> ImplItem {
     let fields_count: usize = fields.db_fields.len();
 
-    let set_db_fields = fields.db_fields.iter().map(|field| {
+    let set_db_fields = fields.db_fields.iter().enumerate().map(|(col_ix, field)| {
         let field_ident = &field.ident;
         let field_type = &field.ty;
 
         quote_spanned! {field.span =>
             #field_ident: {
-                let (col_ix, col_value) = vals_iter
-                    .next()
-                    .unwrap(); // vals_iter size is checked before this code is reached, so
-                               // it is safe to unwrap
-
+                let col_value = ::std::mem::take(&mut row_columns[#col_ix]);
                 <#field_type as charybdis::scylla::FromCqlVal<::std::option::Option<charybdis::scylla::CqlValue>>>::from_cql(col_value)
                     .map_err(|e| charybdis::scylla::FromRowError::BadCqlVal {
                         err: e,
-                        column: col_ix,
+                        column: #col_ix,
                     })?
             },
         }
@@ -36,24 +34,24 @@ pub(crate) fn from_row(struct_name: &syn::Ident, fields: &CharybdisFields) -> Im
     });
 
     let generated = quote! {
-        #[allow(clippy::unwrap_used, clippy::unwrap_in_result)]
         fn from_row(row: charybdis::scylla::Row) -> ::std::result::Result<Self, charybdis::scylla::FromRowError> {
-                use ::std::result::Result::{Ok, Err};
-                use ::std::iter::{Iterator, IntoIterator};
+            use ::std::result::Result::{Ok, Err};
+            use ::std::iter::{Iterator, IntoIterator};
 
-                if #fields_count != row.columns.len() {
-                    return Err(charybdis::scylla::FromRowError::WrongRowSize {
-                        expected: #fields_count,
-                        actual: row.columns.len(),
-                    });
+            let row_columns_len = row.columns.len();
+
+            let mut row_columns: [_; #fields_count] = row.columns.try_into().map_err(|_|
+                charybdis::scylla::FromRowError::WrongRowSize {
+                    expected: #fields_count,
+                    actual: row_columns_len,
                 }
-                let mut vals_iter = row.columns.into_iter().enumerate();
+            )?;
 
-                Ok(#struct_name {
-                    #(#set_db_fields)*
-                    #(#set_other_fields)*
-                })
-            }
+            Ok(#struct_name {
+                #(#set_db_fields)*
+                #(#set_other_fields)*
+            })
+        }
     };
 
     syn::parse_quote!(#generated)
