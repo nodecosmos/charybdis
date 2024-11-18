8f1834f510017f8038ce39d877137385d88891c9
dracarys18 <karthikey.hegde@juspay.in>
2024-11-18 10:25:05 +0530

    fix: add escape characters for all binds

--- charybdis-macros/src/traits/fields/query.rs
+++ charybdis-macros/src/traits/fields/query.rs
@@ -22,7 +22,7 @@ impl FieldsQuery for Vec<&Field<'_>> {
     fn insert_bind_markers(&self) -> String {
         let str_vec = self
             .iter()
-            .map(|field| format!(":{}", field.name))
+            .map(|field| format!(r#":"{}""#, field.name))
             .collect::<Vec<String>>()
             .join(", ");
 
@@ -31,21 +31,21 @@ impl FieldsQuery for Vec<&Field<'_>> {
 
     fn set_bind_markers(&self) -> String {
         self.iter()
-            .map(|field| format!("{} = :{}", field.name, field.name))
+            .map(|field| format!(r#""{}" = :"{}""#, field.name, field.name))
             .collect::<Vec<String>>()
             .join(", ")
     }
 
     fn where_placeholders(&self) -> String {
         self.iter()
-            .map(|field| format!("{} = ?", field.name))
+            .map(|field| format!(r#""{}" = ?"#, field.name))
             .collect::<Vec<String>>()
             .join(" AND ")
     }
 
     fn where_bind_markers(&self) -> String {
         self.iter()
-            .map(|field| format!("{} = :{}", field.name, field.name))
+            .map(|field| format!(r#""{}" = :"{}""#, field.name, field.name))
             .collect::<Vec<String>>()
             .join(" AND ")
     }
