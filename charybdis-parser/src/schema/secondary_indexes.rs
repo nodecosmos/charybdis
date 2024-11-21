a7bc9f12ccc2f71c96bb7a08e54437613bd48453
Goran Brkuljan <goran.brkuljan@gmail.com>
2024-11-21 14:55:19 +0100

    update: fix sec index parsing

--- charybdis-parser/src/schema/secondary_indexes.rs
+++ charybdis-parser/src/schema/secondary_indexes.rs
@@ -46,35 +46,24 @@ impl<'frame, 'metadata> DeserializeValue<'frame, 'metadata> for SecondaryIndex {
                     let target = map[0].1.clone();
 
                     if let CqlValue::Text(target) = target {
-                        // global secondary index
+                        // local secondary index
+                        if target.starts_with('{') {
+                            let parsed: LocalIndexStructure =
+                                serde_json::from_str(&target).expect("Failed to parse local index structure");
+                            let idx = parsed.ck.first().expect("Expected CK key");
 
+                            return Ok(SecondaryIndex {
+                                target: IndexTarget::LocalSecondaryIndex(idx.to_string()),
+                            });
+                        }
+
+                        // global secondary index
                         Ok(SecondaryIndex {
                             target: IndexTarget::GlobalSecondaryIndex(target),
                         })
-                    } else if let CqlValue::Map(map) = target {
-                        // local secondary index
-
-                        let mut ck = Vec::new();
-                        for (key, value) in map {
-                            if let CqlValue::Text(key_str) = key {
-                                // parse only 'ck' key
-                                if key_str == "ck" {
-                                    if let CqlValue::List(ck_list) = value {
-                                        for ck_val in ck_list {
-                                            if let CqlValue::Text(ck_str) = ck_val {
-                                                ck.push(ck_str);
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                        Ok(SecondaryIndex {
-                            target: IndexTarget::LocalSecondaryIndex(ck.first().expect("Expected a string").clone()),
-                        })
                     } else {
                         Err(DeserializationError::new(DbSchemaParserError::TypeError(
-                            "Expected a map for secondary index".to_string(),
+                            "Expected a string".to_string(),
                         )))
                     }
                 } else {
