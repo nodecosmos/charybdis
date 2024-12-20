8e15194be881391f64010ed2fcbbe7230266fd14
Goran Brkuljan <goran.brkuljan@gmail.com>
2024-12-20 01:54:32 +0100

    tests: expose container ports

--- charybdis/tests/integrations/common.rs
+++ charybdis/tests/integrations/common.rs
@@ -1,7 +1,7 @@
 use scylla::{CachingSession, SessionBuilder};
 use tokio::sync::OnceCell;
 
-const NODE: &str = "127.0.0.1:9042";
+const NODES: [&str; 3] = ["127.0.0.1:9042", "127.0.0.1:9043", "127.0.0.1:9044"];
 const KEYSPACE: &str = "charybdis";
 const CACHE_SIZE: usize = 1000;
 
@@ -10,7 +10,7 @@ static ONCE: OnceCell<CachingSession> = OnceCell::const_new();
 pub async fn db_session() -> &'static CachingSession {
     ONCE.get_or_init(|| async {
         let db_session = SessionBuilder::new()
-            .known_node(NODE)
+            .known_nodes(NODES)
             .use_keyspace(KEYSPACE, false)
             .build()
             .await
