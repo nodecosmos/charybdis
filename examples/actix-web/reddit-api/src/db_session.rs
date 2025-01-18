72f1200806a3c3ae2ea3014f60247f4e5174db4c
Goran Brkuljan <goran.brkuljan@gmail.com>
2025-01-18 10:15:04 +0100

    update: add comment to clarify db_session usage

--- examples/actix-web/reddit-api/src/db_session.rs
+++ examples/actix-web/reddit-api/src/db_session.rs
@@ -4,6 +4,7 @@ const NODES: [&str; 3] = ["127.0.0.1:9042", "127.0.0.1:9043", "127.0.0.1:9044"];
 const KEYSPACE: &str = "reddit_example";
 const CACHE_SIZE: usize = 1000;
 
+/// For test use only; do not reinitialize in production.
 pub async fn db_session() -> CachingSession {
     let db_session = default_session().await;
 
